803f1650c771f78db561e9e095b5a09a6cb52d5c
liuyong <2580411920@qq.com>
2023-09-28 17:58:21 +0800

    Changes

--- myPython_study/no6_Python 交换变量.py
+++ myPython_study/no6_Python 交换变量.py
@@ -2,3 +2,18 @@
 https://www.runoob.com/python3/python3-swap-variables.html
 """
 
+# 方法一：使用中间变量
+a0 = 0
+b0 = 1
+temp = a0
+a0 = b0
+b0 = temp
+print(a0, b0)  # 1 0
+
+# 方法二：直接交换
+a1 = 1
+b1 = 2
+a1, b1 = b1, a1
+print(a1, b1)  # 2 1
+
+
