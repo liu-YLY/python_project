73f48d713c0f45d980c2a85b45525715b8fb3269
liuyong <2580411920@qq.com>
2023-10-08 17:47:48 +0800

    Changes

---
+++ myPython_study/no7_Python 获取最大值or最小值函数.py
@@ -0,0 +1,7 @@
+list_a = [1, 2, 3, 4, 5, 6, 7, 8]
+tuple_a = [91, 82, 73, 64, 55, 46, 37, 28]
+set_a = {4, 444, 44, 44444, 444444, 4444444}
+print(max(list_a), min(list_a))  # 8    1
+print(max(tuple_a), min(tuple_a))  # 91    28
+print(max(set_a), min(set_a))  # 4444444    4
+print(max(11, 22, 33, 44, 5, 55, 6, 66, 7), min(11, 22, 33, 44, 5, 55, 6, 66, 7))  # 66    5
