328db7867070a7467f627933d732d2b8b81e8a80
liuyong <2580411920@qq.com>
2023-11-17 16:08:49 +0800

    Changes

--- myTest/selenium中元素属性.py
+++ myTest/selenium中元素属性.py
@@ -4,10 +4,11 @@ import time
 
 driver = webdriver.Chrome()
 driver.get("http://www.baidu.com")
-element = driver.find_element(By.ID, "kw")
+element = driver.find_element(By.ID, "kw").accessible_name
 
 # 打印对象上的属性和方法
-print(dir(driver))
-print(vars(element))
+print(dir(element))
+print(dir(driver.find_element(By.ID, "kw")))
+# print(vars(element))
 
 # print(dir(element))
