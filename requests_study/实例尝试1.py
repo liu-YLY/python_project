2479baeb094a145ac1c239a12845f64e7b062367
liuyong <2580411920@qq.com>
2024-02-05 14:42:35 +0800

    Changes

---
+++ requests_study/实例尝试1.py
@@ -0,0 +1,10 @@
+import requests
+import json
+
+params = {
+    "city": "北京",
+    "key": "xnkd73pN8HHa61J7IWoKLlR0Yk"
+}
+url = "https://qqlykm.cn/api/weather/get"
+WeatherAPIKey = requests.get(url, params=params)
+#WeatherData = json.loads(WeatherAPIKey.text)
